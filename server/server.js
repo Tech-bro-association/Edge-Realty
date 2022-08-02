e450b9dadb1513d5240a3704a82c0babec114b4a
RealRichi3 <Molunorichie@gmail.com>
2022-08-02 11:16:50 +0100

    chore: Initial setup for server.js

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+// const userRoute = require("./routes/userRoutes");
+// const adminRoute = require("./routes/adminRoutes");
+// const agentRoute = require("./routes/agentRoutes");
+const PORT = process.env.PORT || 5000;
+
+// Initialize database
+const uri = "mongodb://localhost:27017/Edge-Realty";
+mongoose.connect(uri);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log(`Connection to ${db.name} database Successful!`);
+});
+
+// Express
+app.use(
+  cors({
+    origin: ["http://localhost:5500", "http://localhost:8080"],
+  })
+);
+app.use(morgan("dev"));
+
+// app.use("/user", userRoute);
+// app.use("/agent", agentRoute);
+// app.use("/admin", adminRoute);
+
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}....`);
+});
