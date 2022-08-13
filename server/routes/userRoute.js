40aa3e739062a28c64490c30ee00504bd70045dd
RealRichi3 <Molunorichie@gmail.com>
2022-08-13 17:54:29 +0100

    chore: Moved and added all password authentication routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,8 +5,6 @@ const userController = require("../controllers/userController");
 
 router.post("/register", userController.addNewUser);
 router.post("/updatedata", userController.updateUserData);
-router.post("/updatepassword", userController.updateUserPassword);
-router.post("/login", userController.loginUser);
 router.post("/find", userController.findUser)
 
 module.exports = router;
