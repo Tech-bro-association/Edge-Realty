0fb30921df7aa2c405c943ca0034ce5b094722ab
RealRichi3 <Molunorichie@gmail.com>
2022-08-13 17:55:43 +0100

    chore: Moved emailService.js and hash.js to /server/controller/utils

--- server/controllers/utils/emailService.js
+++ server/controllers/utils/emailService.js
@@ -1,4 +1,4 @@
-const nodemailer = require("../node_modules/nodemailer/");
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
---
+++ server/controllers/utils/hash.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt');
+const { Password } = require("../../models/passwordModel");
+
+async function hashPassword(password) {
+    let saltRounds = 10
+    await bcrypt.hash(password, saltRounds)
+        .then(hash => password = hash)
+    return password
+}
+
+async function checkHash(password, hash, _res = null) {
+    return await bcrypt.compare(password, hash)
+        .then(response => { return response })
+}
+
+async function saveHash(user_id, user_password) {
+    return new Promise((resolve, reject) => {
+        hashPassword(user_password).then(hash => {
+            let new_password = new Password({
+                user_id_fkey: user_id,
+                password: hash,
+            });
+            new_password.save().then(response => {
+                console.log(response)
+                resolve(response);
+            }).catch(error => {
+                reject(error);
+            });
+        }
+        ).catch(error => {
+            reject(error);
+        }
+        );
+    }); // Promise
+}
+
+module.exports = {
+    hashPassword,
+    saveHash,
+    checkHash,
+};
