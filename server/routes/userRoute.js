51de82e652019259cd397d6a2b664c889b60fe9b
RealRichi3 <Molunorichie@gmail.com>
2022-08-16 19:28:09 +0100

    chore: Added routes for user cart functions

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,5 +5,14 @@ const userController = require("../controllers/userController");
 
 router.post("/register", userController.addNewUser);
 router.post("/updatedata", userController.updateUserData);
+router.post("/cart/add", userController.addPropertyToCart);
+router.post("/cart/remove", userController.removePropertyFromCart);
+router.post("/cart/get", userController.getCartItems);
+router.post("/cart/clear", userController.clearCart);
+router.post("/cart/checkout", userController.checkoutCart);
+router.post("/search", userController.searchProperties);
+router.post("/newsletter", userController.signupForNewsletter);
+
+
 
 module.exports = router;
