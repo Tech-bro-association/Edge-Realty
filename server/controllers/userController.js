fe80e3970e93d612b2055371b836322d6a196c9e
RealRichi3 <Molunorichie@gmail.com>
2022-08-17 01:19:17 +0100

    chore: Added bookappointment function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel").User;
 const { addNewClient, updateClientData } = require("./common/clientsCommonController");
-const { Appointment } = require("../models/appointmentModel");
+const { scheduleAppointment } = require("./common/appointment");
+const { newsletterSignup } = require("../models/newsletterModel");
 
 // Add new user to db
 function addNewUser(req, res) {
@@ -22,7 +23,38 @@ function updateUserData(req, res) {
     updateClientData(res, "user", req.body)
 }
 
-function signupForNewsletter(req, res) { }
+function bookAppointment(req, res) {
+    try {
+        let new_appointment_data = {
+            agent_email_fkey: req.body.agent_email,
+            user_email_fkey: req.body.user_email,
+            date: req.body.date,
+            time: req.body.time,
+            notes: req.body.notes,
+            status: "pending",
+            client_type: "user"
+        }
+        let response = await scheduleAppointment(res, new_appointment_data)
+        if ((response) && (response.length > 0)) {
+            res.status(200).send(response)
+        } else { throw "An error occured" }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+}
+
+async function signupForNewsletter(req, res) {
+    try {
+        let response = await newsletterSignup(req.body)
+        if (response) {
+            res.status(200).send(response)
+        } else { throw "An error occured" }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+}
 
 
 
@@ -30,4 +62,5 @@ module.exports = {
     addNewUser,
     updateUserData,
     signupForNewsletter,
+    bookAppointment
 };
