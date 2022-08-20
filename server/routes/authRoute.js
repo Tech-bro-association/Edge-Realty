dd99b247c1cb7feb82e5f79eff35cae5bf6d1f99
RealRichi3 <Molunorichie@gmail.com>
2022-08-20 02:32:53 +0100

    chore: Removed confirm reset token route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const {resetClientPassword, changeOldPassword, authenticateClientLogin} = require("../controllers/utils/auth/passwordAuth")
-const { confirmResetToken  } = require("../controllers/utils/auth/resetToken")
 
 router.post("/resetpassword", resetClientPassword);
-router.post("/confirmtoken", confirmResetToken);
 router.post("/changepassword", changeOldPassword);
 router.post('/login', authenticateClientLogin);
 
