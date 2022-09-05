5707aa0c97cd99b853241219e777c525318da2eb
Richie <molunorichie@gmail.com>
2022-09-05 02:02:28 +0100

    add: Auth middleware

--- server/middlewares/auth/auth.js
+++ server/middlewares/auth/auth.js
@@ -1,6 +1,16 @@
 const jwt = require("jsonwebtoken");
+const asyncWrapper = require("../asyncWrapper");
+const { UnauthorizedError } = require('../../middlewares/customError')
+const { decodeJWT } = require("../../controllers/utils/jwt");
 
 const config = process.env;
+
+const { Admin, Agent, EndUser } = require('../../models/usersModel');
+const users = {
+  "EndUser": EndUser,
+  "Agent": Agent,
+  "Admin": Admin
+}
 
 const verifyToken = (req, res, next) => {
   const token =
@@ -18,4 +28,22 @@ const verifyToken = (req, res, next) => {
   return next();
 };
 
-module.exports = verifyToken;
+const authenticateRequest = asyncWrapper(async (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer')) { throw new UnauthorizedError('Authentication required') }
+
+  const jwtToken = authHeader.split(' ')[1]
+  const payload = decodeJWT(jwtToken)
+  const User = users[payload.role],
+    currUser = await User.findOne({ _id: payload._id });
+
+  if (!currUser) { throw new UnauthorizedError("Unauthorized access") }
+
+  console.log('Authorized')
+  next()
+})
+
+module.exports = {
+  verifyToken,
+  authenticateRequest
+}
